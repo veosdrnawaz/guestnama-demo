673041bd1d604229b787c0ed67a21671ba61f437
Khalil ahmad <veosdrnwaz@gmail.com>
2025-12-30 11:09:53 +0500

    fix: Update Google Apps Script backend URL
    The previous backend URL was invalid. This commit updates it to the correct deployed URL for the GuestNama Secure Auth application.

--- constants.ts
+++ constants.ts
@@ -3,7 +3,7 @@ import { UserRole } from './types';
 export const APP_NAME = "GuestNama";
 
 // IMPORTANT: Replace this with your deployed Google Apps Script Web App URL
-export const BACKEND_URL = "https://script.google.com/macros/s/AKfycbwRM3jExXyrq5aIsrWT_vUxuREsix7AbcHtmHgubTz-7NxQdF9kv0rSzdDt81nlvxg-rw/exec";
+export const BACKEND_URL = "https://script.google.com/macros/s/AKfycbzjQLqCO8SPJZLamV1duZ7VtjAMVwX4Gajb5UxGF3BKDLZRX1iIUax_kdlD7uQ0e9ORQQ/exec";
 
 export const INITIAL_STORAGE_KEY = "guestnama_db_v1";
 
