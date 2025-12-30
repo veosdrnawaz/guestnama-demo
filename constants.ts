e24e729f2f061c8282ce924d47e6e4fb4c6ceedb
Khalil ahmad <veosdrnwaz@gmail.com>
2025-12-30 10:30:52 +0500

    fix: Update backend URL for production deployment
    The Google Apps Script Web App URL for the backend has been updated to the correct production deployment.

--- constants.ts
+++ constants.ts
@@ -3,7 +3,7 @@ import { UserRole } from './types';
 export const APP_NAME = "GuestNama";
 
 // IMPORTANT: Replace this with your deployed Google Apps Script Web App URL
-export const BACKEND_URL = "https://script.google.com/macros/s/AKfycbx1ZCGSGG4bmzbio-7BHJJ_7UCc6LZiDH3ZqooB3B2zIBoOnLcv1N6bu5FFqqQTlj3zzQ/exec";
+export const BACKEND_URL = "https://script.google.com/macros/s/AKfycbwRM3jExXyrq5aIsrWT_vUxuREsix7AbcHtmHgubTz-7NxQdF9kv0rSzdDt81nlvxg-rw/exec";
 
 export const INITIAL_STORAGE_KEY = "guestnama_db_v1";
 
